36dc6e278b49f01de335abd12cf05c13b134366f
Zainab Jafari <zainabjafari998@gmail.com>
2024-03-25 21:36:09 +0100

    message components added

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import MessageContainer from "./components/Messages/MessageContainer";
 const page = () => {
   return (
     <div className="felx sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
-      {/* <MessageContainer /> */}
+      <MessageContainer />
     </div>
   );
 };
---
+++ frontend/app/components/Messages/message.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Message = () => {
+  return <div>
+    <div>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' />
+				</div>
+			</div>
+			{/* <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>Mmessage.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div> */}
+		</div>
+  </div>;
+};
+
+export default Message;
